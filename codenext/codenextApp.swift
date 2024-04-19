bcdfcd268c34b8c3428af768f5e367e10a7dc1d0
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-19 16:32:32 -0700

    Initial Commit

---
+++ codenext/codenextApp.swift
@@ -0,0 +1,17 @@
+//
+//  codenextApp.swift
+//  codenext
+//
+//  Created by admin on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct codenextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
